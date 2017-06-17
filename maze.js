57000be1d937a433579a62caf8e5d1a12f2af06f
Mathieu PATUREL <australie.p@gmail.com>
2017-06-17 16:21:33 +1000

    :boom: reworked everything. Seems to be working

--- maze.js
+++ maze.js
@@ -1,11 +1,11 @@
 "use strict";
 
+
 // 0 is where you start
 // 1 is where you can go
 // 2 is a wall
 // 3 is where you have to go (the finish line)
 
-// SIMPLE_MAZE → only one posibilty
 
 
 const CODES = {
@@ -15,17 +15,56 @@ const CODES = {
     end: 3
 }
 
-const SIMPLE_MAZE = [
-    [0, 2, 2, 2, 2, 2, 3],
-    [1, 2, 1, 1, 1, 2, 1],
-    [1, 2, 1, 2, 1, 2, 1],
-    [1, 2, 1, 2, 1, 1, 1],
-    [1, 1, 1, 2, 2, 2, 2]
-]
+// SIMPLE_MAZE → only one posibilty
+
+const SIMPLE_MAZE = `
+0222223
+1211121
+1212121
+1212111
+1112222
+`
+
+
+// there is always only 2 possible movement: the one you were before, and an other one
+const COMPLEX_EASY_MAZE = `
+022
+113
+122
+122
+`
+
+// there can be more than 2 possibilities
+
+const COMPLEX_MAZE = `
+011122221
+122122211
+112221112
+211221222
+211111111
+222222213
+`
+
+const COMPLEX_EASY_MAZE_1 = `
+0211111
+1221222
+1111111
+1222221
+1211111
+1222122
+1211113
+`
 
 class Position {
 
     // just a class to use words instead of arrays
+
+    // the following attributes can be set by other classes to a 'Position' instance:
+    // - squareCode        (int)
+    // - explored          (bool)
+    // - moveNeeded        (Array of ints)
+    // - instructionFromIt (Array of arrays of ints)
+    // - isCrossway        (bool)
 
     constructor(x, y) {
         this.x = x
@@ -42,102 +81,162 @@ class Position {
         return this
     }
 
-    isPositive() {
-        return this.x >= 0 && this.y >= 0
-    }
-
     sameAs(position) {
         return position instanceof Position && position.x === this.x && position.y === this.y
     }
 
+    inspect() {
+        return `<Pos ${this.x},${this.y}>`
+    }
 
+    toString() {
+        return this.inspect()
+    }
 }
 
 class Maze {
 
-    constructor(maze) {
-        // maze is an array of arrays of caseCodes
-        this.maze = maze
-        this.height = this.maze.length
-        this.width = this.maze[0].length
+    constructor(plan) {
+        if (typeof plan === 'string') {
+            plan = plan.trim().split('\n').map(bit => bit.split('').map(nb => parseInt(nb)))
+        }
+        this.plan = plan
+    }
 
-        
+    isInPlan(position) {
+        return position.y >= 0 && position.y < this.plan.length
+        && position.x >= 0 && position.x < this.plan[0].length
+    }
+
+    squareCodeFor(position) {
+        if (!this.isInPlan(position)) return null
+            return this.plan[position.y][position.x]
     }
 
     getStartPosition() {
-        for (var i = this.maze.length - 1; i >= 0; i--) {
-            for (var j = this.maze[i].length - 1; j >= 0; j--) {
-                if (this.maze[i][j] === CODES.start) {
+        for (var i = this.plan.length - 1; i >= 0; i--) {
+            for (var j = this.plan[i].length - 1; j >= 0; j--) {
+                if (this.plan[i][j] === CODES.start) {
                     return new Position(j, i)
                 } 
             }
         }
         throw new Error("The maze is invalid, there is no starting position (0)")
     }
-
-    caseCodeFor(position) {
-        if (!position.isPositive()) {
-            return null
-        }
-        return this.maze[position.y][position.x]
-    }
-
-    getPositionAround(position) {
-        // in a clockwise direction
-        return [
-            position.clone().move(0, -1),
-            position.clone().move(1, 0),
-            position.clone().move(0, 1),
-            position.clone().move(-1, 0),
-        ]
-    } 
-
 }
 
 class MazeSolver {
 
     constructor(maze) {
-        // maze is an instance of a Maze
         this.maze = maze
-        this.currentPosition = this.maze.getStartPosition()
+
+        this.currentPosition = maze.getStartPosition().clone()
+        this.exploredPositions = []
+        this.crossways = []
         this.instructions = []
+        this.callCount = 0
     }
 
-    getOut() {
-        // move the perso to the case that is a path and call itself again
-        // or stop if it is on the finish line
+    hasExplored(position, isCrossway=false) {
+        for (var i = this.exploredPositions.length - 1; i >= 0; i--) {
+            if (this.exploredPositions[i].sameAs(position) && !(isCrossway === true && this.exploredPositions[i].isCrossway !== true)) {
+                return true
+            }
+        }
+        return false
+    } 
+
+    getPotentialPositionsAround(position) {
+        // returns a list of positions around 'position' where it is possible to go, and that
+        // haven't been explored yet
         const moves = [
             [-1, 0],
             [0, 1],
             [1, 0],
             [0, -1]
         ]
-        let caseCode, move, position
-        for (let i = moves.length - 1; i >= 0; i--) {
+
+        let move, targetPosition, squareCode, possibleDirections = []
+        for (var i = moves.length - 1; i >= 0; i--) {
             move = moves[i]
-            position = this.currentPosition.clone().move(...move)
-            caseCode = this.maze.caseCodeFor(position)
-            if (caseCode === CODES.end || caseCode === CODES.path && 
-                // so that we don't go back
-                !position.sameAs(this.previousPosition)) {
-                this.previousPosition = this.currentPosition.clone()
-                this.currentPosition.move(...move)
-                this.instructions.push(move)
-                if (caseCode === CODES.end) {
-                    return this.gotOut()
-                } else {
-                    return this.getOut()
-                }
+            targetPosition = this.currentPosition.clone().move(...move)
+            squareCode = this.maze.squareCodeFor(targetPosition)
+            if ((squareCode === CODES.path || squareCode === CODES.end)
+                && !this.hasExplored(targetPosition)) {
+                targetPosition.squareCode = squareCode
+                targetPosition.moveNeeded = move
+                possibleDirections.push(targetPosition)
             }
-            
+        }
+        return possibleDirections
+    }
+
+    moveTo(position) {
+        const instructionRecoreder = (this.crossways.slice(-1)[0] || this)
+        if (instructionRecoreder.instructions === undefined) {
+            instructionRecoreder.instructions = []
+        }
+        instructionRecoreder.instructions.push(position.moveNeeded)
+
+        this.exploredPositions.push(position)
+        this.currentPosition.move(...position.moveNeeded)
+        this.currentPosition.isCrossway = false
+    }
+
+    explore() {
+        this.callCount++
+        if (this.callCount > 50) {
+            return 'call count stop'
+        }
+        let potentialPositionsAround = this.getPotentialPositionsAround(this.currentPosition)
+
+        for (var i = potentialPositionsAround.length - 1; i >= 0; i--) {
+            if (this.maze.squareCodeFor(potentialPositionsAround[i]) === CODES.end) {
+                this.moveTo(potentialPositionsAround[i])
+                this.crossways.map(crossway => this.instructions.push(...crossway.instructions))
+
+                return 'got out'
+            }
+        }
+
+        if (potentialPositionsAround.length === 1) {
+            this.moveTo(potentialPositionsAround[0])
+        }
+
+        else if (potentialPositionsAround.length >= 2) {
+            this.currentPosition.isCrossway = true
+
+            if (!this.hasExplored(this.currentPosition, true)) {
+                this.crossways.push(this.currentPosition.clone())
+            } else {
+                potentialPositionsAround = this.getPotentialPositionsAround(this.crossways.slice(-1)[0]).slice(0)
+            }
+            this.moveTo(potentialPositionsAround[0])
+        }
+
+        else {
+            if (this.crossways.slice(-1)[0].sameAs(this.currentPosition)) {
+                this.crossways.pop()
+            }
+            const crossway = this.crossways.slice(-1)[0]
+            crossway.instructions = []
+            this.currentPosition = crossway.clone()
+        }
+
+        return this.explore()
+    }
+
+    solve() {
+        const message = this.explore()
+        if (message === 'got out') {
+            return this.instructions
+        } else {
+            console.error(`Unexpected return value: "${message}"`)
+            return []
         }
     }
-
-    gotOut() {
-        return this.instructions
-    }
-
 }
+
 
 function mathMoveToWord(moves) {
     const englishMoves = []
@@ -158,12 +257,16 @@ function mathMoveToWord(moves) {
             englishMoves.push('other')
         }
     }
-    return englishMoves
+    return englishMoves.join(', ')
 }
 
 function main() {
-    const solver = new MazeSolver(new Maze(SIMPLE_MAZE))
-    console.log(mathMoveToWord(solver.getOut()).join('\n'))
+
+    const solver = new MazeSolver(new Maze(COMPLEX_MAZE))
+    const instructions = solver.solve()
+    // CSW: ignore
+    console.log('English instructions:', mathMoveToWord(instructions))
+
 }
 
 main()
