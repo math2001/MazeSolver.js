865c848fc4e6ddad3dd2d15e41d45afc70d59751
Mathieu PATUREL <australie.p@gmail.com>
2017-06-21 19:09:21 +1000

    :sparkles: render explored positions

--- render.js
+++ render.js
@@ -12,7 +12,7 @@ class RenderMaze {
         this.context = this.canvas.getContext('2d')
     }
 
-    static renderMaze(maze, path) {
+    static renderMaze(maze, path, exploredPositions) {
         // path is the solution's path
 
         this.canvas.setAttribute('width', this.SQUARE_SIZE * maze.width)
@@ -20,7 +20,13 @@ class RenderMaze {
         this.context.fillStyle = '#eee'
         this.context.fillRect(0, 0, this.SQUARE_SIZE * maze.width, this.SQUARE_SIZE * maze.height)
         
-        this.context.fillStyle = 'rgba(0, 0, 255, 1)'
+        this.context.fillStyle = '#d6edff'
+        for (var i = exploredPositions.length - 1; i >= 0; i--) {
+            this.context.fillRect(exploredPositions[i][0] * this.SQUARE_SIZE, exploredPositions[i][1] * this.SQUARE_SIZE,
+                                  this.SQUARE_SIZE, this.SQUARE_SIZE)
+        }
+
+        this.context.fillStyle = '#55abed'
         for (var i = path.length - 1; i >= 0; i--) {
             this.context.fillRect(path[i][0] * this.SQUARE_SIZE, path[i][1] * this.SQUARE_SIZE,
                                   this.SQUARE_SIZE, this.SQUARE_SIZE)
@@ -32,11 +38,11 @@ class RenderMaze {
             for (var x = 0; x < row.length; x++) {
                 square = row[x]
                 if (square === CODES.wall) {
-                    this.context.fillStyle = 'rgba(0, 0, 0, 1)'
+                    this.context.fillStyle = '#111'
                 } else if (square === CODES.start) {
-                    this.context.fillStyle = 'rgba(0, 255, 0, 1)'
+                    this.context.fillStyle = '#42f465'
                 } else if (square === CODES.end) {
-                    this.context.fillStyle = 'rgba(255, 0, 0, 1)'
+                    this.context.fillStyle = '#f45942'
                 }
                 if (square !== CODES.path) {
                     this.context.fillRect(x * this.SQUARE_SIZE, y * this.SQUARE_SIZE, 
@@ -50,5 +56,6 @@ class RenderMaze {
 }
 
 RenderMaze.init()
-const maze = new Maze(mazes.VICIOUS_4)
-RenderMaze.renderMaze(maze, new MazeSolver(maze).solve('path'))
+const maze = new Maze(mazes.COMPLEX_MAZE_2)
+const mazeSolver =  new MazeSolver(maze)
+RenderMaze.renderMaze(maze, mazeSolver.solve('path'), mazeSolver.exploredPositions.map(pos => pos.toArray()))
